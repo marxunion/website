b59cfd8c8a037505c738587d0be3dbfeeffb8797
cross789 <capsformaps150@proton.me>
2023-07-08 23:51:39 +0200

    Added languages to mobile menu and hiding when clicked.

--- theme/static/js/menu.js
+++ theme/static/js/menu.js
@@ -2,16 +2,18 @@ document.addEventListener("DOMContentLoaded", function() {
     const menuToggle = document.querySelector(".menu-toggle");
     const menu = document.querySelector(".menu");
     const languages = document.querySelector(".languages");
+    const mobileBackground = document.querySelector('.mobile-background');
 
-    menuToggle.addEventListener("click", function() {
+    menuToggle.addEventListener("click", () => {
       menu.classList.toggle("active");
       languages.classList.toggle("active");
+      mobileBackground.classList.toggle('active');
     });
-  });
 
-  var hamburgerButton = document.getElementById('hamburger-button');
-  var menuToggle = document.querySelector('.menu-toggle');
-  
-  hamburgerButton.addEventListener('click', function() {
-    menuToggle.classList.toggle('active');
+    const menuItemsList = document.querySelector(".menu-items-list");
+    menuItemsList.addEventListener("click", (event) => {
+        menu.classList.toggle("active");
+        languages.classList.toggle("active");
+        mobileBackground.classList.toggle('active');
+    })
   });
