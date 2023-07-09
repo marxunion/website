e1b69c642861e8e45ea5e9a25f28b2efb496cf5f
cross789 <capsformaps150@proton.me>
2023-07-09 18:23:27 +0200

    Fixed mobile menu backround position, added closing of menu on background click, made languages clickable.

--- theme/static/js/menu.js
+++ theme/static/js/menu.js
@@ -1,19 +1,32 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", () => {
     const menuToggle = document.querySelector(".menu-toggle");
     const menu = document.querySelector(".menu");
     const languages = document.querySelector(".languages");
     const mobileBackground = document.querySelector('.mobile-background');
 
-    menuToggle.addEventListener("click", () => {
-      menu.classList.toggle("active");
-      languages.classList.toggle("active");
-      mobileBackground.classList.toggle('active');
+    function closeMenu(){
+        menu.classList.toggle("active");
+        languages.classList.toggle("active");
+        mobileBackground.classList.toggle('active');
+    }
+
+    menuToggle.addEventListener("click",  () => {
+        closeMenu();
     });
 
     const menuItemsList = document.querySelector(".menu-items-list");
     menuItemsList.addEventListener("click", (event) => {
-        menu.classList.toggle("active");
-        languages.classList.toggle("active");
-        mobileBackground.classList.toggle('active');
-    })
+        console.log('menuItemsList');
+        closeMenu();
+    });
+    const mobileMenuBackground = document.querySelector(".mobile-background");
+    mobileMenuBackground.addEventListener("click", (event) => {
+        console.log('mobileMenuBackground');
+        closeMenu();
+    });
+    const languagesList = document.querySelector('.languages-list');
+    languagesList.addEventListener("click", (event) => {
+        console.log('languagesList');
+        closeMenu();
+    });
   });
