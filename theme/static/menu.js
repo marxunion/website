8234592f22c1b21fd5afd9bc46b27850aa4d58d8
cross789 <capsformaps150@proton.me>
2023-08-08 11:42:35 +0200

    Removed js from mobile menu.

--- theme/static/menu.js
+++ theme/static/menu.js
@@ -1,30 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const menuToggle = document.querySelector(".menu-toggle");
-    const menu = document.querySelector(".menu");
+    const menuToggle = document.querySelector("#mobile-menu-toggle");
     const mobileBackground = document.querySelector(".mobile-background");
 
     function closeMenu(){
-        menu.classList.toggle("active");
-        mobileBackground.classList.toggle("active");
+        menuToggle.checked = false;
     }
 
-    menuToggle.addEventListener("click",  () => {
-        closeMenu();
-    });
-
-    const menuItemsList = document.querySelector(".menu-items-list");
-    menuItemsList.addEventListener("click", (event) => {
-        console.log("menuItemsList");
-        closeMenu();
-    });
-    const mobileMenuBackground = document.querySelector(".mobile-background");
-    mobileMenuBackground.addEventListener("click", (event) => {
-        console.log("mobileMenuBackground");
-        closeMenu();
-    });
-    const languagesList = document.querySelector(".languages-list");
-    languagesList.addEventListener("click", (event) => {
-        console.log("languagesList");
+    mobileBackground.addEventListener("click",  () => {
         closeMenu();
     });
   });
